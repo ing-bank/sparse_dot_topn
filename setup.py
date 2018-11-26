7a7bab2462171e4c170208d25baa3f5ed4577511
ymwdalex <sunzs@8c8590159c40.ant.amazon.com>
2018-11-26 23:13:18 +0100

    v0.2.1: fix the problem when pip install by tar.gz; update meta-information of package

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+from os import path
 import numpy
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
+
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 ext_utils = Extension('sparse_dot_topn.sparse_dot_topn',
                       sources=['./sparse_dot_topn/sparse_dot_topn.pyx', './sparse_dot_topn/sparse_dot_topn_source.cpp'],
@@ -11,19 +19,24 @@ ext_utils = Extension('sparse_dot_topn.sparse_dot_topn',
                       language='c++',
                      )
 
-setup(name='sparse_dot_topn',
-      version='0.2',
-      description='This package boosts a sparse matrix multiplication '\
-                  'followed by selecting the top-n multiplication',
-      keywords='cosine-similarity sparse-matrix scipy cython',
-      setup_requires=[
-          # Setuptools 18.0 properly handles Cython extensions.
-          'setuptools>=18.0',
-          'cython',
-          'numpy'
-      ],
-      packages=['sparse_dot_topn'],
-      cmdclass={'build_ext': build_ext},
-      ext_modules=cythonize([ext_utils]),
-     )
+setup(
+    name='sparse_dot_topn',
+    version='0.2.1',
+    description='This package boosts a sparse matrix multiplication '\
+                'followed by selecting the top-n multiplication',
+    keywords='cosine-similarity sparse-matrix scipy cython',
+    long_description=long_description,
+    url='https://github.com/ing-bank/sparse_dot_topn',
+    author='Zhe Sun',
+    author_email='ymwdalex@gmail.com',
+    setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        'setuptools>=18.0',
+        'cython',
+        'numpy'
+    ],
+    packages=['sparse_dot_topn'],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize([ext_utils]),
+)
 
--- sparse_dot_topn/__init__.py
+++ sparse_dot_topn/__init__.py
@@ -1,2 +1,1 @@
 from sparse_dot_topn.awesome_cossim_topn import awesome_cossim_topn
-__version__ = '0.2'
