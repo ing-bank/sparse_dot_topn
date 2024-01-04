52d5e5ca5e9245b55c901b11e3d6d575e9cc8abb
Ralph Urlus <rurlus.dev@gmail.com>
2024-01-04 15:59:06 +0100

    BLD: [C++] Search for Homebrew OpenMP on ARM MacOS

--- src/sparse_dot_topn_core/cmake/FindDependencies.cmake
+++ src/sparse_dot_topn_core/cmake/FindDependencies.cmake
@@ -1,14 +1,22 @@
 # -- Python
-find_package(Python 3.8
-  REQUIRED COMPONENTS Interpreter Development.Module
+find_package(
+  Python 3.8 REQUIRED
+  COMPONENTS Interpreter Development.Module
   OPTIONAL_COMPONENTS Development.SABIModule)
 
 # -- Nanobind
 find_package(nanobind CONFIG REQUIRED)
 
+
 # -- OpenMP
-if(SDTN_ENABLE_OPENMP)
-  find_package(OpenMP REQUIRED)
-elseif(NOT SDTN_DISABLE_OPENMP)
+if(NOT SDTN_DISABLE_OPENMP)
   find_package(OpenMP)
+  if ((NOT OpenMP_FOUND) AND APPLE)
+    include(SetHomebrew)
+    set(OpenMP_ROOT ${HOMEBREW_PREFIX}/opt/libomp)
+    find_package(OpenMP)
+  endif()
+  if (NOT OpenMP_FOUND AND SDTN_ENABLE_OPENMP)
+    find_package(OpenMP REQUIRED)
+  endif()
 endif()
---
+++ src/sparse_dot_topn_core/cmake/SetHomebrew.cmake
@@ -0,0 +1,11 @@
+
+if(NOT DEFINED ${HOMEBREW_PREFIX})
+  if(DEFINED ENV{HOMEBREW_PREFIX} AND IS_DIRECTORY ENV{HOMEBREW_PREFIX})
+    set(HOMEBREW_PREFIX ENV{HOMEBREW_PREFIX})
+  elseif(IS_DIRECTORY /opt/homebrew)
+    set(HOMEBREW_PREFIX /opt/homebrew)
+  else()
+    set(HOMEBREW_PREFIX /usr/local)
+  endif()
+endif()
+
