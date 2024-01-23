ee585471b9b3c4786b1c7a93b329fd2f5e1dfb55
Ralph Urlus <rurlus.dev@gmail.com>
2024-01-23 20:04:11 +0100

    ENH: Add sp_matmul and pass through when topn is equal to number of columns

--- src/sparse_dot_topn/__init__.py
+++ src/sparse_dot_topn/__init__.py
@@ -2,8 +2,8 @@
 import importlib.metadata
 
 __version__ = importlib.metadata.version("sparse_dot_topn")
-from sparse_dot_topn.api import awesome_cossim_topn, sp_matmul_topn
+from sparse_dot_topn.api import awesome_cossim_topn, sp_matmul, sp_matmul_topn
 from sparse_dot_topn.lib import _sparse_dot_topn_core as _core
 from sparse_dot_topn.lib._sparse_dot_topn_core import _has_openmp_support
 
-__all__ = ["awesome_cossim_topn", "sp_matmul_topn", "_core", "__version__", "_has_openmp_support"]
+__all__ = ["awesome_cossim_topn", "sp_matmul", "sp_matmul_topn", "_core", "__version__", "_has_openmp_support"]
