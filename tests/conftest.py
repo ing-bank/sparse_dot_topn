b2af08fb764c06871ec6f2593cb519a974e3b000
Ralph Urlus <rurlus.dev@gmail.com>
2024-04-09 15:09:48 +0200

    TST: Use different seed to avoid ordering conflict due to identical samples being drawn

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,4 @@ import pytest
 
 @pytest.fixture(scope="package")
 def rng():
-    return np.random.Generator(np.random.PCG64DXSM(1483336117))
+    return np.random.Generator(np.random.PCG64DXSM(1481236117))
